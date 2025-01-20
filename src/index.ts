3b9676f85e4de014e1858386d47106cc0f1209e6
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-21 02:13:08 +0530

    added connection handling to WebSocket server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { WebSocketServer } from 'ws'; 
 
 const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function(socket){
+    console.log("New client connected");
+    socket.send("Welcome to the server");  
+    socket.on('message', (e)=>{
+        console.log(e.toString());
+    })
+})
