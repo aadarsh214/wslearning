e6c53e9192888bb71fca7175d058885ff94deb4d
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-25 16:51:09 +0530

    feat: implement WebSocket ping-pong communication and update App component for message sending

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,11 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function(socket){
     console.log("New client connected");
-    socket.send("Welcome to the server");  
+
     socket.on('message', (e)=>{
-        console.log(e.toString());
+        if(e.toString() === 'ping'){
+            socket.send('pong');
+        }
+        
     })
 })      
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
 export default function App(){
+  const [ws, setWs] = useState<WebSocket>();
+  function sendMessage(){
+    ws.send("ping")
+  }
+
+  useEffect(() =>{
+    const ws = new WebSocket('ws://localhost:8080/');
+    setWs(ws);
+    ws.onmessage = (event) => {
+      console.log(event.data)
+    }
+  },[])
+
+
   return (
     <div>
-      <h1>App</h1>
+     <input type="text" placeholder="Message.." />
+     <button onClick={sendMessage}>send</button>
     </div>
   )
 }
